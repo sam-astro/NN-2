6ab21c7018a6afe11fc26d9401961784c80ed332
sam-astro <77079540+sam-astro@users.noreply.github.com>
2022-03-26 22:48:26 -0400

    sf kjsdn

--- Assets/Scripts/NetManager.cs
+++ Assets/Scripts/NetManager.cs
@@ -72,18 +72,12 @@ public class NetManager : MonoBehaviour
 
     public GameObject netEntityPrefab;
 
-    AudioMgr audMgr;
-
     double[] correctData;
 
     #endregion
 
     private void Start()
     {
-        double[] l = new double[0];
-        double[] r = new double[0];
-        audMgr = new AudioMgr();
-        audMgr.openWav("./Assets/inAudio2.wav", out l, out r);
         correctData = l;
 
         maxIterations = l.Length;
@@ -401,94 +395,3 @@ public class NetManager : MonoBehaviour
         Console.ResetColor();
     }
 }
-
-class AudioMgr
-{
-    //static Audio myAudio = new Audio();
-    private static byte[] myWaveData;
-
-    // Sample rate (Or number of samples in one second)
-    private const int SAMPLE_FREQUENCY = 44100;
-
-    public void SaveAudio(double[] input, double errorRate)
-    {
-        List<Byte> tempBytes = new List<byte>();
-
-        WaveHeader header = new WaveHeader();
-        FormatChunk format = new FormatChunk();
-        DataChunk data = new DataChunk();
-
-        SoundGenerator ldat = new SoundGenerator(SAMPLE_FREQUENCY, input);
-        SoundGenerator rdat = new SoundGenerator(SAMPLE_FREQUENCY, input);
-
-        data.AddSampleData(ldat.Data, rdat.Data);
-
-        header.FileLength += format.Length() + data.Length();
-
-        tempBytes.AddRange(header.GetBytes());
-        tempBytes.AddRange(format.GetBytes());
-        tempBytes.AddRange(data.GetBytes());
-
-        myWaveData = tempBytes.ToArray();
-
-        System.IO.File.WriteAllBytes("./Assets/outputAudioFiles/" + errorRate + ".wav", myWaveData);
-    }
-
-    // convert two bytes to one double in the range -1 to 1
-    static double bytesToDouble(byte firstByte, byte secondByte)
-    {
-        // convert two bytes to one short (little endian)
-        //short s = (secondByte << 8) | firstByte;
-        short s = BitConverter.ToInt16(new byte[2] { (byte)firstByte, (byte)secondByte }, 0);
-        // convert to range from -1 to (just below) 1
-        return s / 32760.0;
-    }
-
-    // Returns left and right double arrays. 'right' will be null if sound is mono.
-    public void openWav(string filename, out double[] left, out double[] right)
-    {
-        byte[] wav = File.ReadAllBytes(filename);
-
-        // Determine if mono or stereo
-        int channels = wav[22];     // Forget byte 23 as 99.999% of WAVs are 1 or 2 channels
-
-        // Get past all the other sub chunks to get to the data subchunk:
-        int pos = 12;   // First Subchunk ID from 12 to 16
-
-        // Keep iterating until we find the data chunk (i.e. 64 61 74 61 ...... (i.e. 100 97 116 97 in decimal))
-        while (!(wav[pos] == 100 && wav[pos + 1] == 97 && wav[pos + 2] == 116 && wav[pos + 3] == 97))
-        {
-            pos += 4;
-            int chunkSize = wav[pos] + wav[pos + 1] * 256 + wav[pos + 2] * 65536 + wav[pos + 3] * 16777216;
-            pos += 4 + chunkSize;
-        }
-        pos += 8;
-
-        // Pos is now positioned to start of actual sound data.
-        int samples = (wav.Length - pos) / 2;     // 2 bytes per sample (16 bit sound mono)
-        if (channels == 2) samples /= 2;        // 4 bytes per sample (16 bit stereo)
-
-        // Allocate memory (right will be null if only mono sound)
-        left = new double[samples];
-        if (channels == 2)
-            right = new double[samples];
-        else
-            right = null;
-
-        // Write to double array/s:
-        int i = 0;
-        while (pos < samples * 4)
-        {
-            left[i] = bytesToDouble(wav[pos], wav[pos + 1]);
-            //Console.WriteLine(left[i]);
-            pos += 2;
-            if (channels == 2)
-            {
-                right[i] = bytesToDouble(wav[pos], wav[pos + 1]);
-                pos += 2;
-            }
-            i++;
-        }
-    }
-}
-
